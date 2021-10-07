08fe00df882354da3900474ff2c9026be798c029
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-08 04:13:44 +0530

    Refactored: To combine all rules into regex

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,14 +1,14 @@
-const EMAIL_PATTERN = RegExp("((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$");
+const EMAIL_PATTERN = RegExp("^((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$");
 
 
 class EmailValidator {
 
     vaildateEmail(emailValue) {
         if (EMAIL_PATTERN.test(emailValue)) {
-            console.log(("Email " + emailValue + " is  valid"));
+            console.log(("Email: " + emailValue + " is valid"));
         }
         else {
-            console.log(("Email " + emailValue + " is  not valid"));
+            console.log(("Email: " + emailValue + " is not valid"));
         }
     }
 }
@@ -17,7 +17,7 @@ class EmailValidator {
 let emailValidator = new EmailValidator();
 
 let allMails = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
-    "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com"
+    "abc.100@abc.com.au", "abc@1.com", "abc+100@gmail.com", "abc", "abc@gmail.com.com", "abc@.com.my", "abc123@gmail.a", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com"
     , "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", ".abc@gmail.com.aa.au"];
 
 allMails.forEach(email => {
